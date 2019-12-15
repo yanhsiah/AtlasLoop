fd5aca2c319b748b35f4d57a278d6c8612b89901
yallenh <yanhsianghuang@gmail.com>
2019-12-14 17:10:42 -0800

    125. Valid Palindrome

---
+++ Q125_ValidPalindrome.swift
@@ -0,0 +1,32 @@
+//
+//  Q125_ValidPalindrome.swift
+//  AtlasLoop
+//
+//  Created by 黃彥翔 on 2019/12/14.
+//
+
+import Foundation
+
+class ValidPalindrome {
+    func isPalindrome(_ s: String) -> Bool {
+        var start = 0, end = s.count - 1
+        let s = Array(s.lowercased())
+        while start < end {
+            while start < end && !s[start].isalnum { start += 1 }
+            while start < end && !s[end].isalnum { end -= 1 }
+            if s[start] != s[end] {
+                return false
+            } else {
+                start += 1
+                end -= 1
+            }
+        }
+        return true
+    }
+}
+
+extension Character {
+    var isalnum: Bool {
+        return isLetter || isNumber
+    }
+}
