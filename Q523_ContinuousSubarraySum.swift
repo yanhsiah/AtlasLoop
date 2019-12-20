0b14d8e73736dca45a143a6620c5bf9acc61ab38
yallenh <yanhsianghuang@gmail.com>
2019-12-20 12:44:17 -0800

    523. Continuous Subarray Sum

---
+++ Q523_ContinuousSubarraySum.swift
@@ -0,0 +1,27 @@
+//
+//  Q523_ContinuousSubarraySum.swift
+//  AtlasLoop
+//
+//  Created by 黃彥翔 on 2019/12/20.
+//
+
+import Foundation
+
+class ContinuousSubarraySum {
+    // time: O(N), space: O(N)
+    func checkSubarraySum(_ nums: [Int], _ k: Int) -> Bool {
+        var indexes = [0: -1]
+        var remainder = 0
+        for i in 0..<nums.count {
+            remainder += nums[i]
+            if k != 0 { remainder %= k }
+            
+            if let index = indexes[remainder] {
+                if i - index > 1 { return true }
+            } else {
+                indexes[remainder] = i
+            }
+        }
+        return false
+    }
+}
