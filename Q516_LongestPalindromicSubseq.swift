8f177d808030930c8306cd9e823434b542a8172f
yallenh <yanhsianghuang@gmail.com>
2019-12-28 00:35:15 -0800

    516. Longest Palindromic Subsequence

---
+++ Q516_LongestPalindromicSubseq.swift
@@ -0,0 +1,65 @@
+//
+//  Q516_LongestPalindromicSubseq.swift
+//  AtlasLoop
+//
+//  Created by 黃彥翔 on 2019/12/28.
+//
+
+import Foundation
+
+class LongestPalindromicSubseq {
+    
+    // time: O(2^N), space: O(2^N) for brute force
+    // time: O(N^2), space: O(N^2) for memorization
+    func longestPalindromeSubseq(_ s: String) -> Int {
+        return dfs(0, s.count - 1, Array(s))
+    }
+    var cache = [Int: [Int: Int]]()  ///
+    func dfs(_ l: Int, _ r: Int, _ s: [Character]) -> Int {
+        if l == r { return 1 }
+        if l > r { return 0 }
+        if let rights = cache[l], let res = rights[r] { return res } ///
+        let res = s[l] == s[r] ? (2+dfs(l+1, r-1, s)) : max(dfs(l+1, r, s), dfs(l, r-1, s))
+        cache[l, default: [Int: Int]()][r] = res
+        return res
+    }
+    
+    /*
+    func longestPalindromeSubseq(_ s: String) -> Int {
+    
+        // time: O(N^2), space: O(N^2)
+        let s = Array(s)
+        let n = s.count
+        var dp = Array(repeating: Array(repeating: 0, count: n), count: n)
+        for i in stride(from: n-1, through: 0, by: -1) {
+            dp[i][i] = 1
+            for j in i+1..<n {
+                if s[i] == s[j] {
+                    dp[i][j] = dp[i+1][j-1] + 2
+                } else {
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+                }
+            }
+        }
+        return dp[0][n-1]
+
+        // time: O(N^2), space: O(N)
+        // let s = Array(s)
+        // let n = s.count
+        // var dp = Array(repeating: 0, count: n)
+        // for i in stride(from: n-1, through: 0, by: -1) {
+        //     var ndp = Array(repeating: 0, count: n)
+        //     ndp[i] = 1
+        //     for j in i+1..<n {
+        //         if s[i] == s[j] {
+        //             ndp[j] = dp[j-1] + 2
+        //         } else {
+        //             ndp[j] = max(dp[j], ndp[j-1])
+        //         }
+        //     }
+        //     swap(&ndp, &dp)
+        // }
+        // return dp[n-1]
+    }
+    */
+}
