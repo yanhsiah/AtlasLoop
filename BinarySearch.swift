aa7595094701791ad5cf1ed404131f4c79fd2e4e
yallenh <yanhsianghuang@gmail.com>
2019-12-29 09:08:10 -0800

    binary search

--- BinarySearch.swift
+++ BinarySearch.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+extension Array where Element == Int {
+    // return Index of the first element >= target
+    func lowerBound(_ target: Int) -> Int {
+        var start = 0, end = self.count - 1
+        while start < end {
+            let m = start + (end - start) / 2
+            if self[m] < target { start = m + 1 }
+            else { end = m }
+        }
+        return start
+    }
+    // return Index of the first element > target
+    func upperBound(_ target: Int) -> Int {
+        var start = 0, end = self.count - 1
+        while start < end {
+            let m = start + (end - start) / 2
+            if self[m] <= target { start = m + 1 }
+            else { end = m }
+        }
+        return start
+    }
+    // return Index of the insert position
+    func findInsertPos(_ target: Int) -> Int {
+        var start = 0, end = self.count - 1
+        while start <= end {
+            let m = start + (end - start) / 2
+            if self[m] == target { return m }
+            if self[m] < target { start = m + 1 }
+            else { end = m - 1 }
+        }
+        return start
+    }
+}
