fbb0719c85fb3dc2449f5376455986f1b95b745f
yallenh <yanhsianghuang@gmail.com>
2020-01-04 12:03:57 -0800

    987. Vertical Order Traversal of a Binary Tree

--- Q987_VerticalOrderTraversal.swift
+++ Q987_VerticalOrderTraversal.swift
@@ -14,27 +14,28 @@ class VerticalOrderTraversal {
     // If two nodes have the same position, the value of the node
     // that is reported first is the value that is smaller.
     // time: O(NlogN), space: O(N)
-    typealias Cache = [Int: [Int: [Int]]]
+
+    // typealias Cache = [Int: [Int: [Int]]]
+    var cache = [Int: [Int: [Int]]]() // [x: y: [val]]
     func verticalTraversal(_ root: TreeNode?) -> [[Int]] {
-        var cache = Cache()
-        dfs(&cache, root, 0, 0)
+        dfs(root, 0, 0)
         
         var res = [[Int]]()
         for x in cache.keys.sorted() {
             guard let cols = cache[x] else { continue }
-            var col = [Int]()
+            var vals = [Int]()
             for y in cols.keys.sorted() {
                 guard let nodes = cols[y] else { continue }
-                col += nodes.sorted()
+                vals += nodes.sorted()
             }
-            res.append(col)
+            res.append(vals)
         }
         return res
     }
-    func dfs(_ cache: inout Cache, _ node: TreeNode?, _ x: Int, _ y: Int) {
+    func dfs(_ node: TreeNode?, _ x: Int, _ y: Int) {
         guard let node = node else { return }
-        cache[x, default: [Int: [Int]]()][y, default: [Int]()].append(node.val)
-        dfs(&cache, node.left, x-1, y+1)
-        dfs(&cache, node.right, x+1, y+1)
+        cache[x, default: [:]][y, default: []].append(node.val)
+        dfs(node.left, x-1, y+1)
+        dfs(node.right, x+1, y+1)
     }
 }
