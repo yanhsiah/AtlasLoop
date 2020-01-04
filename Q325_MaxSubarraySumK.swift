59484a30e1b5fb27187875b8d0009367a03bb875
yallenh <yanhsianghuang@gmail.com>
2020-01-04 15:08:35 -0800

    325. Maximum Size Subarray Sum Equals k

---
+++ Q325_MaxSubarraySumK.swift
@@ -0,0 +1,26 @@
+//
+//  Q325_MaxSubarraySumK.swift
+//  AtlasLoop
+//
+//  Created by 黃彥翔 on 2020/1/4.
+//
+
+import Foundation
+
+class MaxSubarraySumK {
+    
+    // 560
+    // time: O(N), space: O(N)
+    func maxSubArrayLen(_ nums: [Int], _ k: Int) -> Int {
+        var dict = [0: -1] // prefix sum: index
+        var res = 0, sum = 0
+        for (i, num) in nums.enumerated() {
+            sum += num
+            if let index = dict[sum - k] {
+                res = max(res, i - index)
+            }
+            if dict[sum] == nil { dict[sum] = i }
+        }
+        return res
+    }
+}
