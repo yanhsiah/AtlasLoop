c5604a7a3b11a6f924783b7caa8b76777aa32cc2
yallenh <yanhsianghuang@gmail.com>
2020-01-06 00:18:56 -0800

    721. Accounts Merge

--- Q721_AccountsMerge.swift
+++ Q721_AccountsMerge.swift
@@ -8,29 +8,25 @@
 import Foundation
 
 class DSU {
-    var parents = [Int](), ranks = [Int]()
+    private var parents = [Int](), ranks = [Int]()
+    func add() -> Int {
+        let i = parents.count;
+        parents.append(i); ranks.append(0)
+        return i
+    }
     func union(_ a: Int, _ b: Int) {
         let a = find(a), b = find(b)
         if a != b {
-            if ranks[a] < ranks[b] {
-                parents[a] = b
-            } else {
-                parents[b] = a
-                if ranks[a] == ranks[b] { ranks[a] += 1 }
-            }
+            if ranks[a] < ranks[b] { parents[a] = b }
+            else { parents[b] = a; if ranks[a] == ranks[b] { ranks[a] += 1 } }
         }
     }
     func find(_ x: Int) -> Int {
-        var x = x
+        var x = x;
         while parents[x] != x {
-            parents[x] = parents[parents[x]]
-            x = parents[x]
+            parents[x] = parents[parents[x]]; x = parents[x]
         }
         return x
-    }
-    func add(_ i: Int) {
-        parents.append(i)
-        ranks.append(0)
     }
 }
 
@@ -45,21 +41,18 @@ class AccountsMerge {
         for i in 0..<accounts.count {
             let a = accounts[i]
             guard a.count > 1 else { continue }
+            let owner = a[0]
             for j in 1..<a.count {
-                if emailInfo[a[j]] == nil {
-                    let eid = emailInfo.count
-                    emailInfo[a[j]] = (eid, a[0])
-                    dsu.add(eid)
-                }
+                if emailInfo[a[j]] == nil { emailInfo[a[j]] = (dsu.add(), owner) }
                 if j > 1 { dsu.union(emailInfo[a[j]]!.0, emailInfo[a[1]]!.0) }
             }
         }
-
+        
         var unions = [Int: [String]]()
         for (email, (eid, _)) in emailInfo {
             unions[dsu.find(eid), default: []].append(email)
         }
-        return unions.map { [emailInfo[$0.value[0]]!.1] + $0.value.sorted() }
+        return unions.map { [ emailInfo[$0.value[0]]!.1 ] + $0.value.sorted() }
     }
 }
 
